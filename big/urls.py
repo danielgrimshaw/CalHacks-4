871458fc49f5bfed2f92d21c5cca0128bd64bae2
Daniel Grimshaw <danielgrimshaw@berkeley.edu>
2017-10-08 00:58:46 -0700

    Add endpoint to upload process

--- big/urls.py
+++ big/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^upload/$', views.upload, name='upload'),
+        url(r'^thanks/(?P<datasetId>[0-9]+)/$', views.complete, name='complete'),
 ]
--- big/views.py
+++ big/views.py
@@ -10,9 +10,15 @@ def upload(request):
     if request.method == "POST":
         form = UploadDataset(request.POST, request.FILES)
         if form.is_valid():
-            handleUpload(form.cleaned_data['email'], request.FILES['dataset'])
-            HttpResponseRedirect(reverse('complete'))
+            datasetId = handleUpload(form.cleaned_data['email'], request.FILES['dataset'])
+            HttpResponseRedirect(reverse('complete', args=(datasetId,)))
     else:
         form = UploadDataset()
 
     return render(request, 'big/upload.html', { 'form': form })
+
+def handleUpload(email, dataset):
+    pass
+
+def complete(request, datasetId):
+    return render(request, 'big/thanks.html', { 'dataset': Dataset.objects.get(pk=datasetId) })
