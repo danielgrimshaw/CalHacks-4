4dd34edd1511e2d994e75964093dcb8a89d2de24
Daniel Grimshaw <danielgrimshaw@berkeley.edu>
2017-10-08 00:31:05 -0700

    Add barebones form

--- big/views.py
+++ big/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import UploadDataset
+
 def index(request):
     return render(request, 'big/index.html', None)
+
+def upload(request):
+    if request.method == "POST":
+        form = UploadDataset(request.POST, request.FILES)
+        if form.is_valid():
+            handleUpload(form.cleaned_data['email'], request.FILES['dataset'])
+            HttpResponseRedirect(reverse('complete'))
+    else:
+        form = UploadDataset()
+
+    return render(request, 'big/upload.html', { 'form': form })
